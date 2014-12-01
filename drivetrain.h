caeb378315d26be171f22c1de22d97e5f18d4540
JulianKnodt <julianknodt@gmail.com>
2014-11-30 16:13:43 -0800

    Update drivetrain.h
    added empty functions to drivetrain.h

---
+++ drivetrain.h
@@ -0,0 +1,23 @@
+#ifndef DRIVETRAIN_H
+#ifndef DRIVETRAIN_H
+#include <WPILib.h>
+#include <stdlib.h>
+#include <iostream>
+
+class drivetrain{
+  private:
+    Victor leftFrontVic;
+    Victor leftBackVic;
+    Victor rightFrontVic;
+    Victor rightBackVic;
+  
+  public:
+    drivetrain():
+    void init();
+    void disable();
+    void driveD();
+    void setSpeed();
+    void stop();
+    void update();
+}
+#endif

6d5b8097e8afe67daf2a967cfe7bb3f4efa50648
JulianKnodt <julianknodt@gmail.com>
2014-12-04 15:13:09 -0800

    Created and added empty methods
    The constructors are for creating the driveTrain, accumulator, Shooter.

---
+++ Command.h
@@ -0,0 +1,22 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+#include "WPILib.h"
+#include "humanController.h"
+#include "drivetrain.h"
+class command
+{
+    private:
+        drivetrain *trainpointer;
+        HumanController *humanPointer;
+	      typedef enum TransferType
+	      {
+			DRIVETRAIN,
+			SHOOTER,
+			ACCUMULATOR
+	  	} transferType;
+	  public:
+	    void transfer(TransferType transferType);
+	  CommandSys(DriveTrain *trainPointer, HumanController *humanPointer);
+		CommandSys(Accumulator *accumulatorPointer, HumanController *humanPointer);
+		CommandSys(Shooter *shooterPointer, HumanController *humanPointer);
+};

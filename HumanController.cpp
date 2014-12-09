5d87d8872bd531c6c8ad5ec23eae6fe6bfa3b380
JulianKnodt <julianknodt@gmail.com>
2014-12-09 14:36:44 -0800

    Filled in methods from the header file

--- HumanController.cpp
+++ HumanController.cpp
@@ -1,1 +1,25 @@
+#include HumanController.h
+HumanController::HumanController():
+X((uint32_t)2),
+Y((uint32_t)1)
+{
+horizontalValue = 0;
+verticalValue = 0;
 
+}
+double HumanController::getHorizontalValue()
+{
+  horizontalValue = X.getX();
+  return horizontalValue;
+}
+double HumanController::getVerticalValue()
+{
+	verticalValue = Y.GetY();
+	return verticalValue;
+}
+
+void HumanController::update()
+{
+	getVerticalValue();
+	getHorizontalValue();
+}

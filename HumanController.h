d72e3de82c68b80fdb14f14cf273bf75b60080be
JulianKnodt <julianknodt@gmail.com>
2014-12-09 14:16:53 -0800

    Naming Consistencies

--- HumanController.h
+++ HumanController.h
@@ -1,14 +1,14 @@
 #ifndef HUMANCONTROLLER_H
 #define HUMANCONTROLLER_H
 #include "WPILib.h"
-class humanController{
+class HumanController{
     private:
       double horizontalValue;
       double verticalValue;
       Joystick X;
       Joystick Y;
     public:
-      humanController();
+      HumanController();
       double getHorizontal();
       double getVertical();
       void update();
